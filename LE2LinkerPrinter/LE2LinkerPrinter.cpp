0875ecc2c978b04dbc510ed438bbee471ea71e3f
Mgamerz <mgamerz2@gmail.com>
2022-05-30 23:47:29 -0600

    Add LE2 SeqAct_LogEnabler

--- LE2LinkerPrinter/LE2LinkerPrinter.cpp
+++ LE2LinkerPrinter/LE2LinkerPrinter.cpp
@@ -3,4 +3,4 @@
 
 #include "../../Shared-ASI/LinkerPrinter/LELinkerPrinter.h"
 
-// ASI code is all defined in LELinkerPrinter.h
+// ASI code is all defined in LELinkerPrinter.h in LEASIMods repo
---
+++ LE2SeqAct_LogEnabler/LE2SeqAct_LogEnabler.cpp
@@ -0,0 +1,6 @@
+// Game
+#define GAMELE2
+
+#include "../../Shared-ASI/SeqAct_LogEnabler/SeqActLogEnabler.h"
+
+// ASI code is all defined in SeqActLogEnabler.h in the LEASIMods repo
